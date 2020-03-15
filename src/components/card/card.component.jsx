22e39fb6ffa5c5aad460ffe3cef773b8f202434b
Michał Kowieski <kowieski.m98@gmail.com>
2020-03-15 16:21:16 +0100

    Add img element to card component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -6,6 +6,7 @@ export const Card = (props) => {
     console.log(props)
     return (
         <div className='card'>
+            <img alt="user" src={`https://robohash.org/${props.user.id}/?set=set4`}/>
             <h1> {props.user.name} </h1>
         </div>
     )
