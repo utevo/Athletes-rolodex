3f910e12b8443d1e91ca1236838485837ea9b83f
Michał Kowieski <kowieski.m98@gmail.com>
2020-03-15 20:10:14 +0100

    Add company.name and email as rendered fields of card component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -8,6 +8,8 @@ export const Card = (props) => {
         <div className='card'>
             <img alt="user" src={`https://robohash.org/${props.user.id}/?set=set4`}/>
             <h1> {props.user.name} </h1>
+            <p> {props.user.company.name} </p>
+            <p> {props.user.email} </p>
         </div>
     )
 }
