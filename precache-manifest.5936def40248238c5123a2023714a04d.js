1a74c4a82c160c810106b5577700bc8dc7cdc024
Michał Kowieski <kowieski.m98@gmail.com>
2020-03-21 21:13:50 +0100

    Updates

---
+++ precache-manifest.5936def40248238c5123a2023714a04d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "570009ae859ada478e2a668d6f8690fa",
+    "url": "/Rolodex/index.html"
+  },
+  {
+    "revision": "9dc26b000dfcf8e8722b",
+    "url": "/Rolodex/static/css/main.a43a5172.chunk.css"
+  },
+  {
+    "revision": "b0824a9d426e4e1d0a4c",
+    "url": "/Rolodex/static/js/2.af2b92c5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Rolodex/static/js/2.af2b92c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dc26b000dfcf8e8722b",
+    "url": "/Rolodex/static/js/main.11dc18e2.chunk.js"
+  },
+  {
+    "revision": "691f9c24bc81fe207df4",
+    "url": "/Rolodex/static/js/runtime-main.da4c6e3b.js"
+  }
+]);
